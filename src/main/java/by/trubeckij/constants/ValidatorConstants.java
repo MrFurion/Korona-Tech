b20e012ace73643466c3e2efbbce887da444eb65
Trubetski <trubetski.a@gmail.com>
2025-08-11 11:15:02 +0300

    corected code ++

--- src/main/java/by/trubeckij/constants/ValidatorConstants.java
+++ src/main/java/by/trubeckij/constants/ValidatorConstants.java
@@ -9,7 +9,6 @@ public class ValidatorConstants {
     public static final String INVALID_ORDER_TYPE = "Invalid order type: ";
     public static final String INVALID_OUTPUT_TYPE = "Invalid output type: ";
     public static final String OUTPUT_FILE_SPECIFIED_WITHOUT_PATH = "Output file specified without --path=";
-    public static final String PATH_SPECIFIED_WITHOUT_OUTPUT_FILE = "Path specified without -output=file";
     public static final String OUTPUT_OR_PATH_SPECIFIED_WITHOUT_STAT = "Output or path specified without --stat";
     public static final String UNKNOWN_PARAMETER = "Unknown parameter: ";
     public static final String WHEN_USING_OUTPUT_FILE_YOU_MUST_SPECIFY_A_NON_EMPTY_PATH_WITH_PATH_PATH = "When using --output=file, you must specify a non-empty path with --path=<path>";
